5a2d7046ea59d59741b1d7adad31fba3bdf8e5db
CoreDX1 <chrismaquis@gmail.com>
2023-03-21 09:27:18 -0300

    Elimando las key de Lsp-zero

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,7 +1,7 @@
 local lsp = require("lsp-zero")
 lsp.preset({
-	name = "minimal",
-	set_lsp_keymaps = false,
+	name = 'minimal',
+	set_lsp_keymaps = true,
 	manage_nvim_cmp = true,
 	suggest_lsp_servers = false,
 })
